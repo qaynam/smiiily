f89812037879f366a2532eed0f00f96aec19bdbe
qaynam <hebibullajan@gamil.com>
2023-08-14 21:55:14 +0900

    add constants variables for roundness

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,17 +1,26 @@
-export type PaddingType = 'small' | 'medium' | 'large';
+export type PaddingType = "small" | "medium" | "large";
+export type RoundnessType = "none" | "small" | "medium" | "large";
 
 export const gradients = [
-  'bg-gradient-to-l from-violet-700 via-indigo-300 to-cyan-400',
-  'bg-gradient-to-tl from-fuchsia-300 via-blue-300 to-red-400',
-  'bg-gradient-to-l from-cyan-100 via-purple-300 to-white',
-  'bg-gradient-to-br from-transparent via-orange-100 to-white',
-  'bg-gradient-to-t from-cyan-600 via-cyan-400 to-indigo-100'
+  "bg-gradient-to-l from-violet-700 via-indigo-300 to-cyan-400",
+  "bg-gradient-to-tl from-fuchsia-300 via-blue-300 to-red-400",
+  "bg-gradient-to-l from-cyan-100 via-purple-300 to-white",
+  "bg-gradient-to-br from-transparent via-orange-100 to-white",
+  "bg-gradient-to-t from-cyan-600 via-cyan-400 to-indigo-100"
 ];
 
 export const paddingTypes = {
-  small: 'p-5',
-  medium: 'p-10',
-  large: 'p-20'
+  small: "p-5",
+  medium: "p-10",
+  large: "p-20"
+};
+
+export const roundnessTypes = {
+  none: "rounded-none",
+  small: "rounded-md",
+  medium: "rounded-xl",
+  large: "rounded-[1.5rem]"
 };
 
 export const paddingKeys = Object.keys(paddingTypes) as PaddingType[];
+export const roundnessKeys = Object.keys(roundnessTypes) as RoundnessType[];
