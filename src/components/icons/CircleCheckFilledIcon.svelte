5bfdf731580946a29382a382c0f10d58a53ac458
qaynam <hebibullajan@gamil.com>
2023-08-14 19:01:58 +0900

    update icon props

--- src/components/icons/CircleCheckFilledIcon.svelte
+++ src/components/icons/CircleCheckFilledIcon.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
-  export let fillColor = "none";
+  let classes = "";
+
+  export { classes as class };
 </script>
 
 <svg
   xmlns="http://www.w3.org/2000/svg"
+  class={classes}
   width="24"
   height="24"
   viewBox="0 0 24 24"
   stroke-width="2"
   stroke="currentColor"
-  fill={fillColor}
+  fill="none"
   stroke-linecap="round"
   stroke-linejoin="round"
 >
--- src/components/icons/CircleXFilledIcon.svelte
+++ src/components/icons/CircleXFilledIcon.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
-  export let fillColor = "red";
+  let classes = "";
+
+  export { classes as class };
 </script>
 
 <svg
   xmlns="http://www.w3.org/2000/svg"
   width="24"
   height="24"
+  class={classes}
   viewBox="0 0 24 24"
   stroke-width="2"
   stroke="currentColor"
-  fill={fillColor}
+  fill="none"
   stroke-linecap="round"
   stroke-linejoin="round"
 >
