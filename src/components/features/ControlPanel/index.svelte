5cb3a306b736fa1a6176a52bd7ad1cd1405ac8f2
qaynam <hebibullajan@gamil.com>
2023-08-14 19:24:53 +0900

    update control panel component

--- src/components/features/ControlPanel/index.svelte
+++ src/components/features/ControlPanel/index.svelte
@@ -2,17 +2,19 @@
   import { clsx } from "clsx";
   import { createEventDispatcher } from "svelte";
   import { twMerge } from "tailwind-merge";
-  import { paddingKeys, paddingTypes, type PaddingType } from "~/constants";
+  import { gradients, paddingKeys, paddingTypes, type PaddingType } from "~/constants";
   import Button from "../../basic/Button.svelte";
   import Card from "../../basic/Card.svelte";
   import Stack from "../../basic/Stack.svelte";
   import CopyIcon from "../../icons/CopyIcon.svelte";
   import DownloadIcon from "../../icons/DownloadIcon.svelte";
-  import { toastStore } from "~/stores";
 
   type EventParams = {
     paddingChange: {
       padding: PaddingType;
+    };
+    gradientChange: {
+      gradientIndex: number;
     };
     copy: undefined;
     save: undefined;
@@ -21,11 +23,32 @@
   const dispatch = createEventDispatcher<EventParams>();
 
   export let padding = paddingTypes.small;
+  export let currentGradient = 0;
+  export let onCopy: () => void | Promise<void> = () => void 0;
+  export let onSave: () => void | Promise<void> = () => void 0;
 
   const selectPaddingHandler = (paddingType: PaddingType) => {
     dispatch("paddingChange", {
       padding: paddingType
     });
+  };
+
+  const selectGradientHandler = (gradientIndex: number) => {
+    dispatch("gradientChange", {
+      gradientIndex
+    });
+  };
+
+  const copyHandler = async (event: CustomEvent<{ target: HTMLButtonElement }>) => {
+    event.detail.target.disabled = true;
+    await onCopy();
+    event.detail.target.disabled = false;
+  };
+
+  const saveHandler = async (event: CustomEvent<{ target: HTMLButtonElement }>) => {
+    event.detail.target.disabled = true;
+    await onSave();
+    event.detail.target.disabled = false;
   };
 </script>
 
@@ -46,39 +69,33 @@
         {/each}
       </div>
     </Stack>
-    <!-- <Stack class="gap-2">
+    <Stack class="gap-2">
       <span class="font-bold text-white">Colors</span>
       <div class="flex flex-wrap gap-4">
         {#each gradients as gradientItem, index}
           <button
             class={twMerge(
               gradientItem,
-              'w-10 h-10 rounded-lg',
+              "w-10 h-10 rounded-lg",
               clsx({
-                'ring-2 ring-white': currentGradient === index
+                "ring-2 ring-white": currentGradient === index
               })
             )}
-            on:click={() => changeGradient(index)}
+            on:click={() => selectGradientHandler(index)}
           />
         {/each}
       </div>
-    </Stack> -->
+    </Stack>
     <hr class="border-gray-600" />
     <div class="flex justify-between gap-4">
-      <Button class="w-full" outline on:click={() => dispatch("copy")}>
+      <Button class="w-full" outline on:click={copyHandler}>
         <CopyIcon slot="icon" />
         <span> Copy </span>
       </Button>
-      <Button
-        class="bg-blue-600 hover:bg-blue-500 text-white w-full"
-        on:click={() => dispatch("save")}
-      >
+      <Button class="bg-blue-600 hover:bg-blue-500 text-white w-full" on:click={saveHandler}>
         <DownloadIcon slot="icon" />
         <span> Save </span>
       </Button>
-      <Button class="text-white" on:click={() => toastStore.show("あちゃぁああ", "error", 5000)}
-        >toast</Button
-      >
     </div>
   </Stack>
 </Card>
