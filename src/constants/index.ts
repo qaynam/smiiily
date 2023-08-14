1da334f4f8b576b70952713442c4aa3459beb1da
qaynam <hebibullajan@gmail.com>
2023-08-15 00:39:11 +0900

    add more gradation options

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,8 +5,12 @@ export const gradients = [
   "bg-gradient-to-l from-violet-700 via-indigo-300 to-cyan-400",
   "bg-gradient-to-tl from-fuchsia-300 via-blue-300 to-red-400",
   "bg-gradient-to-l from-cyan-100 via-purple-300 to-white",
+  "bg-gradient-to-r from-green-300 to-purple-400",
+  "bg-gradient-to-r from-indigo-300 to-purple-400",
+  "bg-gradient-to-t from-cyan-600 via-cyan-400 to-indigo-100",
+  "bg-gradient-to-r from-rose-100 to-teal-100",
   "bg-gradient-to-br from-transparent via-orange-100 to-white",
-  "bg-gradient-to-t from-cyan-600 via-cyan-400 to-indigo-100"
+  "bg-gradient-to-r from-gray-700 via-gray-900 to-black"
 ];
 
 export const paddingTypes = {
