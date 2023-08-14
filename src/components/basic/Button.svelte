e0e3e1e49b7b6661c0d9b85f7610991a522aa634
qaynam <hebibullajan@gmail.com>
2023-08-14 15:51:06 +0900

    update button component

--- src/components/basic/Button.svelte
+++ src/components/basic/Button.svelte
@@ -2,6 +2,7 @@
   import { clsx } from 'clsx';
   import { createEventDispatcher } from 'svelte';
   import type { MouseEventHandler } from 'svelte/elements';
+  import { twMerge } from 'tailwind-merge';
 
   const dispatch = createEventDispatcher<{
     click: {
@@ -19,14 +20,19 @@
 
   const defaultClass = 'px-3 py-2 rounded-lg font-semibold';
 
-  $: buttonClass = clsx(defaultClass, classes, {
-    'bg-transparent border border-gray-500 text-gray-200': outline,
-    'hover:bg-gray-900 hover:border-gray-400': outline
-  });
+  $: hasIcon = !!$$slots.icon;
+  $: buttonClass = twMerge(
+    clsx(defaultClass, classes, {
+      'bg-transparent border border-gray-500 text-gray-200': outline,
+      'hover:bg-gray-900 hover:border-gray-400': outline,
+      'flex items-center justify-center gap-2': hasIcon
+    })
+  );
 
   export { classes as class, outline };
 </script>
 
 <button class={buttonClass} on:click={clickHandler}>
+  <slot name="icon" />
   <slot />
 </button>
