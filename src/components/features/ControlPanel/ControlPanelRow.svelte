7b6c8a260add3ebfa4f1ec78475bfcd50c4f6d86
qaynam <hebibullajan@gamil.com>
2023-08-14 21:53:55 +0900

    create control panel row component

---
+++ src/components/features/ControlPanel/ControlPanelRow.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { SvelteComponent } from "svelte";
+  import Stack from "~/components/basic/Stack.svelte";
+
+  export let labelIcon: new (...args: any[]) => SvelteComponent;
+  export let label: string;
+</script>
+
+<Stack class="gap-4">
+  <span class="font-bold text-white flex gap-1">
+    <svelte:component this={labelIcon} />
+    {label}
+  </span>
+  <slot />
+</Stack>
