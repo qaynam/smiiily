72a5e0942f543dba2df5ca1800bfa6ad31632bef
qaynam <hebibullajan@gamil.com>
2023-08-14 19:24:35 +0900

    update Button Component

--- src/components/basic/Button.svelte
+++ src/components/basic/Button.svelte
@@ -1,31 +1,31 @@
 <script lang="ts">
-  import { clsx } from 'clsx';
-  import { createEventDispatcher } from 'svelte';
-  import type { MouseEventHandler } from 'svelte/elements';
-  import { twMerge } from 'tailwind-merge';
+  import { clsx } from "clsx";
+  import { createEventDispatcher } from "svelte";
+  import type { MouseEventHandler } from "svelte/elements";
+  import { twMerge } from "tailwind-merge";
 
   const dispatch = createEventDispatcher<{
     click: {
-      event: MouseEvent;
+      target: HTMLButtonElement;
     };
   }>();
 
-  let classes = '';
+  let classes = "";
   let outline = false;
-  let clickHandler: MouseEventHandler<HTMLButtonElement> = () => {
-    dispatch('click', {
-      event: new MouseEvent('click')
+  let clickHandler: MouseEventHandler<HTMLButtonElement> = (e) => {
+    dispatch("click", {
+      target: e.currentTarget
     });
   };
 
-  const defaultClass = 'px-3 py-2 rounded-lg font-semibold';
+  const defaultClass = "px-3 py-2 rounded-lg font-semibold";
 
   $: hasIcon = !!$$slots.icon;
   $: buttonClass = twMerge(
     clsx(defaultClass, classes, {
-      'bg-transparent border border-gray-500 text-gray-200': outline,
-      'hover:bg-gray-900 hover:border-gray-400': outline,
-      'flex items-center justify-center gap-2': hasIcon
+      "bg-transparent border border-gray-500 text-gray-200": outline,
+      "hover:bg-gray-900 hover:border-gray-400": outline,
+      "flex items-center justify-center gap-2": hasIcon
     })
   );
 
