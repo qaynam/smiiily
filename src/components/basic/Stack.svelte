4cde8abac9341886e7d1af54556f84c369c7fff3
qaynam <hebibullajan@gamil.com>
2023-08-14 19:24:20 +0900

    fix stack class prop bug

--- src/components/basic/Stack.svelte
+++ src/components/basic/Stack.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import { clsx } from "clsx";
+  import { twMerge } from "tailwind-merge";
 
   let classes = "";
 
   const defaultClasses = "flex flex-col gap-4";
 
-  $: stackClasses = clsx(defaultClasses, classes);
+  $: stackClasses = twMerge(clsx(defaultClasses, classes));
 
   export { classes as class };
 </script>
