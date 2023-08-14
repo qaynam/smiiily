8ea9c7f6c3250bc3c07aee0c96cd90e67d53ce0c
qaynam <hebibullajan@gamil.com>
2023-08-14 21:54:16 +0900

    refactor control panel and add remove image button

--- src/components/features/ControlPanel/index.svelte
+++ src/components/features/ControlPanel/index.svelte
@@ -2,12 +2,26 @@
   import { clsx } from "clsx";
   import { createEventDispatcher } from "svelte";
   import { twMerge } from "tailwind-merge";
-  import { gradients, paddingKeys, paddingTypes, type PaddingType } from "~/constants";
+  import {
+    gradients,
+    paddingKeys,
+    paddingTypes,
+    type PaddingType,
+    roundnessKeys,
+    roundnessTypes,
+    type RoundnessType
+  } from "~/constants";
   import Button from "../../basic/Button.svelte";
   import Card from "../../basic/Card.svelte";
   import Stack from "../../basic/Stack.svelte";
   import CopyIcon from "../../icons/CopyIcon.svelte";
   import DownloadIcon from "../../icons/DownloadIcon.svelte";
+  import BoxPaddingIcon from "~/components/icons/BoxPaddingIcon.svelte";
+  import RadiusTopLeftIcon from "~/components/icons/RadiusTopLeftIcon.svelte";
+  import PaletteIcon from "~/components/icons/PaletteIcon.svelte";
+  import ControlPanelRow from "./ControlPanelRow.svelte";
+  import RotateClockWiseIcon from "~/components/icons/RotateClockWiseIcon.svelte";
+  import { toastStore } from "~/stores";
 
   type EventParams = {
     paddingChange: {
@@ -16,6 +30,9 @@
     gradientChange: {
       gradientIndex: number;
     };
+    roundnessChange: {
+      roundness: RoundnessType;
+    };
     copy: undefined;
     save: undefined;
   };
@@ -23,11 +40,15 @@
   const dispatch = createEventDispatcher<EventParams>();
 
   export let padding = paddingTypes.small;
-  export let currentGradient = 0;
+  export let roundness = roundnessTypes.small;
+  export let gradientIndex = 0;
+  export let imageSelected = false;
   export let onCopy: () => void | Promise<void> = () => void 0;
   export let onSave: () => void | Promise<void> = () => void 0;
+  export let onRemoveImage: () => void | Promise<void> = () => void 0;
 
   const selectPaddingHandler = (paddingType: PaddingType) => {
+    console.log(padding);
     dispatch("paddingChange", {
       padding: paddingType
     });
@@ -36,6 +57,12 @@
   const selectGradientHandler = (gradientIndex: number) => {
     dispatch("gradientChange", {
       gradientIndex
+    });
+  };
+
+  const selectRoundnessHandler = (roundnessType: RoundnessType) => {
+    dispatch("roundnessChange", {
+      roundness: roundnessType
     });
   };
 
@@ -53,10 +80,25 @@
 </script>
 
 <Card class="border border-gray-600 w-3/12 self-start ">
-  <Stack class="gap-6">
-    <Stack class="gap-2">
-      <span class="font-bold text-white">Padding</span>
-      <div class="gap-2 flex flex-wrap">
+  <Stack class="gap-8">
+    <ControlPanelRow labelIcon={RadiusTopLeftIcon} label="Roundness">
+      <div class="gap-3 flex flex-wrap">
+        {#each roundnessKeys as roundnessKey}
+          <button
+            class={twMerge(
+              clsx("text-white", {
+                "border-b border-white": roundness === roundnessTypes[roundnessKey]
+              })
+            )}
+            on:click={() => selectRoundnessHandler(roundnessKey)}
+          >
+            {roundnessKey.replace(/^\w/, (c) => c.toUpperCase())}
+          </button>
+        {/each}
+      </div>
+    </ControlPanelRow>
+    <ControlPanelRow labelIcon={BoxPaddingIcon} label="Padding">
+      <div class="gap-3 flex flex-wrap">
         {#each paddingKeys as paddingKey}
           <button
             class={clsx("text-white", {
@@ -68,24 +110,23 @@
           </button>
         {/each}
       </div>
-    </Stack>
-    <Stack class="gap-2">
-      <span class="font-bold text-white">Colors</span>
-      <div class="flex flex-wrap gap-4">
+    </ControlPanelRow>
+    <ControlPanelRow labelIcon={PaletteIcon} label="Gradients">
+      <div class="flex flex-wrap gap-3">
         {#each gradients as gradientItem, index}
           <button
             class={twMerge(
               gradientItem,
               "w-10 h-10 rounded-lg",
               clsx({
-                "ring-2 ring-white": currentGradient === index
+                "ring-2 ring-white": gradientIndex === index
               })
             )}
             on:click={() => selectGradientHandler(index)}
           />
         {/each}
       </div>
-    </Stack>
+    </ControlPanelRow>
     <hr class="border-gray-600" />
     <div class="flex justify-between gap-4">
       <Button class="w-full" outline on:click={copyHandler}>
@@ -97,5 +138,11 @@
         <span> Save </span>
       </Button>
     </div>
+    {#if imageSelected}
+      <Button outline on:click={onRemoveImage}>
+        <RotateClockWiseIcon slot="icon" />
+        <span> Remove Image </span>
+      </Button>
+    {/if}
   </Stack>
 </Card>
