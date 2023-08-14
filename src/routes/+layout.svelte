ddc87920ad30fa058e332c146acaab7e19beef3b
qaynam <hebibullajan@gamil.com>
2023-08-14 21:55:39 +0900

    add footer for page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
+  import Stack from "~/components/basic/Stack.svelte";
   import ToastSlot from "~/components/features/ToastSlot.svelte";
   import "~/styles/app.css";
 </script>
 
-<slot />
+<main class="min-h-[100vh] bg-slate-950 relative pt-10 flex flex-col">
+  <slot />
+  <footer class="mt-auto mb-6 text-center">
+    <Stack>
+      <span class="text-white">
+        <a class="underline" target="_blank" href="https://www.buymeacoffee.com/qaynam"
+          >Buy Me a Coffee 🍵</a
+        >
+      </span>
+      <span class="text-gray-500">
+        &copy; 2021 <a class="underline" href="/">Smiiily</a>
+      </span>
+    </Stack>
+  </footer>
+</main>
 <ToastSlot />
