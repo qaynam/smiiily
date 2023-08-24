dd9db08bd999c1c4802c7a67f3965a37c7e626c7
qaynam <hebibullajan@gmail.com>
2023-08-24 19:16:51 +0900

    change buy me a coffee background

--- src/views/components/features/BuyMeACoffeeBlock.svelte
+++ src/views/components/features/BuyMeACoffeeBlock.svelte
@@ -1,10 +1,68 @@
 <div
-  class="text-white p-6 bg-zinc-900 hover:bg-zinc-800/70 border border-zinc-700 rounded-xl mx-auto lg:w-auto w-full items-center justify-center"
+  id="bmac"
+  class="border border-white bg-zinc-900 hover:bg-zinc-800/70 rounded-xl mx-auto lg:w-auto w-full items-center justify-center"
 >
-  <div class="flex flex-col space-y-6">
+  <div class="flex flex-col p-4 rounded-xl space-y-6 bg-transparent backdrop-blur-xl">
     <span class="block font-semibold text-center"> If You Like This App, Buy Me A Coffee 😋 </span>
     <a target="_blank" href="https://www.buymeacoffee.com/qaynam">
-      <img src="/buy-me-a-coffee.png" alt="buy me a coffee" class="w-44 mx-auto" />
+      <img
+        src="/buy-me-a-coffee.png"
+        alt="buy me a coffee"
+        class="w-44 mx-auto border border-gray-300/30 rounded-lg shadow hover:opacity-90 transition duration-300 ease-in-out"
+      />
     </a>
   </div>
 </div>
+
+<style>
+  #bmac {
+    background: #02abd1;
+    background-image: radial-gradient(at 66% 95%, hsl(231, 88%, 57%) 0px, transparent 50%),
+      radial-gradient(at 99% 94%, hsl(64, 83%, 30%) 0px, transparent 50%),
+      radial-gradient(at 25% 61%, hsl(198, 70%, 67%) 0px, transparent 50%),
+      radial-gradient(at 100% 32%, hsl(285, 64%, 67%) 0px, transparent 50%),
+      radial-gradient(at 68% 22%, hsl(135, 97%, 85%) 0px, transparent 50%);
+    background-blend-mode: normal, normal, normal, normal, normal;
+    background-size: 100% 100%, 100% 100%, 100% 100%, 100% 100%, 100% 100%;
+    background-position: 0% 50%, 100% 50%, 0% 50%, 100% 50%, 0% 50%;
+    animation: background-gradient-animation 10s ease infinite;
+  }
+
+  @keyframes background-gradient-animation {
+    0% {
+      background-image: radial-gradient(at 66% 95%, hsl(231, 88%, 57%) 0px, transparent 50%),
+        radial-gradient(at 99% 94%, hsl(64, 83%, 30%) 0px, transparent 50%),
+        radial-gradient(at 25% 61%, hsl(198, 70%, 67%) 0px, transparent 50%),
+        radial-gradient(at 100% 32%, hsl(285, 64%, 67%) 0px, transparent 50%),
+        radial-gradient(at 68% 22%, hsl(135, 97%, 85%) 0px, transparent 50%);
+    }
+    25% {
+      background-image: radial-gradient(at 66% 95%, hsl(231, 88%, 57%) 0px, transparent 50%),
+        radial-gradient(at 99% 94%, hsl(64, 83%, 30%) 0px, transparent 50%),
+        radial-gradient(at 25% 61%, hsl(198, 70%, 67%) 0px, transparent 50%),
+        radial-gradient(at 100% 32%, hsl(285, 64%, 67%) 0px, transparent 50%),
+        radial-gradient(at 68% 22%, hsl(135, 97%, 85%) 0px, transparent 50%);
+    }
+    50% {
+      background-image: radial-gradient(at 66% 95%, hsl(231, 88%, 57%) 0px, transparent 50%),
+        radial-gradient(at 99% 94%, hsl(64, 83%, 30%) 0px, transparent 50%),
+        radial-gradient(at 25% 61%, hsl(198, 70%, 67%) 0px, transparent 50%),
+        radial-gradient(at 100% 32%, hsl(285, 64%, 67%) 0px, transparent 50%),
+        radial-gradient(at 68% 22%, hsl(135, 97%, 85%) 0px, transparent 50%);
+    }
+    75% {
+      background-image: radial-gradient(at 66% 95%, hsl(231, 88%, 57%) 0px, transparent 50%),
+        radial-gradient(at 99% 94%, hsl(64, 83%, 30%) 0px, transparent 50%),
+        radial-gradient(at 25% 61%, hsl(198, 70%, 67%) 0px, transparent 50%),
+        radial-gradient(at 100% 32%, hsl(285, 64%, 67%) 0px, transparent 50%),
+        radial-gradient(at 68% 22%, hsl(135, 97%, 85%) 0px, transparent 50%);
+    }
+    100% {
+      background-image: radial-gradient(at 66% 95%, hsl(231, 88%, 57%) 0px, transparent 50%),
+        radial-gradient(at 99% 94%, hsl(64, 83%, 30%) 0px, transparent 50%),
+        radial-gradient(at 25% 61%, hsl(198, 70%, 67%) 0px, transparent 50%),
+        radial-gradient(at 100% 32%, hsl(285, 64%, 67%) 0px, transparent 50%),
+        radial-gradient(at 68% 22%, hsl(135, 97%, 85%) 0px, transparent 50%);
+    }
+  }
+</style>
