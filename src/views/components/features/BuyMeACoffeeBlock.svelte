aa742a66569adb18b39020755f2b696f81df6afe
qaynam <hebibullajan@gmail.com>
2023-08-24 19:55:15 +0900

    remove keyframe style

--- src/views/components/features/BuyMeACoffeeBlock.svelte
+++ src/views/components/features/BuyMeACoffeeBlock.svelte
@@ -22,47 +22,5 @@
       radial-gradient(at 25% 61%, hsl(198, 70%, 67%) 0px, transparent 50%),
       radial-gradient(at 100% 32%, hsl(285, 64%, 67%) 0px, transparent 50%),
       radial-gradient(at 68% 22%, hsl(135, 97%, 85%) 0px, transparent 50%);
-    background-blend-mode: normal, normal, normal, normal, normal;
-    background-size: 100% 100%, 100% 100%, 100% 100%, 100% 100%, 100% 100%;
-    background-position: 0% 50%, 100% 50%, 0% 50%, 100% 50%, 0% 50%;
-    animation: background-gradient-animation 10s ease infinite;
-  }
-
-  @keyframes background-gradient-animation {
-    0% {
-      background-image: radial-gradient(at 66% 95%, hsl(231, 88%, 57%) 0px, transparent 50%),
-        radial-gradient(at 99% 94%, hsl(64, 83%, 30%) 0px, transparent 50%),
-        radial-gradient(at 25% 61%, hsl(198, 70%, 67%) 0px, transparent 50%),
-        radial-gradient(at 100% 32%, hsl(285, 64%, 67%) 0px, transparent 50%),
-        radial-gradient(at 68% 22%, hsl(135, 97%, 85%) 0px, transparent 50%);
-    }
-    25% {
-      background-image: radial-gradient(at 66% 95%, hsl(231, 88%, 57%) 0px, transparent 50%),
-        radial-gradient(at 99% 94%, hsl(64, 83%, 30%) 0px, transparent 50%),
-        radial-gradient(at 25% 61%, hsl(198, 70%, 67%) 0px, transparent 50%),
-        radial-gradient(at 100% 32%, hsl(285, 64%, 67%) 0px, transparent 50%),
-        radial-gradient(at 68% 22%, hsl(135, 97%, 85%) 0px, transparent 50%);
-    }
-    50% {
-      background-image: radial-gradient(at 66% 95%, hsl(231, 88%, 57%) 0px, transparent 50%),
-        radial-gradient(at 99% 94%, hsl(64, 83%, 30%) 0px, transparent 50%),
-        radial-gradient(at 25% 61%, hsl(198, 70%, 67%) 0px, transparent 50%),
-        radial-gradient(at 100% 32%, hsl(285, 64%, 67%) 0px, transparent 50%),
-        radial-gradient(at 68% 22%, hsl(135, 97%, 85%) 0px, transparent 50%);
-    }
-    75% {
-      background-image: radial-gradient(at 66% 95%, hsl(231, 88%, 57%) 0px, transparent 50%),
-        radial-gradient(at 99% 94%, hsl(64, 83%, 30%) 0px, transparent 50%),
-        radial-gradient(at 25% 61%, hsl(198, 70%, 67%) 0px, transparent 50%),
-        radial-gradient(at 100% 32%, hsl(285, 64%, 67%) 0px, transparent 50%),
-        radial-gradient(at 68% 22%, hsl(135, 97%, 85%) 0px, transparent 50%);
-    }
-    100% {
-      background-image: radial-gradient(at 66% 95%, hsl(231, 88%, 57%) 0px, transparent 50%),
-        radial-gradient(at 99% 94%, hsl(64, 83%, 30%) 0px, transparent 50%),
-        radial-gradient(at 25% 61%, hsl(198, 70%, 67%) 0px, transparent 50%),
-        radial-gradient(at 100% 32%, hsl(285, 64%, 67%) 0px, transparent 50%),
-        radial-gradient(at 68% 22%, hsl(135, 97%, 85%) 0px, transparent 50%);
-    }
   }
 </style>
